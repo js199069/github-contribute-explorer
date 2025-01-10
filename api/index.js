f9bf0bbc7a2caf8883df203eff67ea62ea491184
jalilseif <jalilseif10@gmail.com>
2025-01-10 17:26:54 +0330

    make express server less to deploy on vercel

---
+++ api/index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const serverless = require('serverless-http');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to Express Boilerplate API!' });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong!' });
+});
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
+});
+
+const handler = serverless(app);
+
+module.exports.handler = handler;
